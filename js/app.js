487ec0b0762345023f3be54b028f601eec00c59a
Cezar Tosta <cezar.tosta@hotmail.com>
2024-03-29 20:58:01 -0300

    comprar() e comprarPista()
    Ajustes em comprar()
    Nova função comprarPista()

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 function comprar(){
     let tipo = document.getElementById('tipo-ingresso');
-    let qtd = document.getElementById('qtd');
-    
+    let qtd = parseInt(document.getElementById('qtd').value); // Quando traz da página, é uma string. Tem que converter para inteiro então
+
+    if(tipo.value == 'pista'){
+        comprarPista(qtd);
+    }
+}
+
+function comprarPista(qtd){
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista){
+        alert('Quantidade indisponível para a Pista!');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
 }
